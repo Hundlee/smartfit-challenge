c729138750f5862b6446ac6916c84b21ebb22ecf
Hundlee <mesquitag884@gmail.com>
2024-02-09 11:41:32 -0300

    feat: add Welcome components

---
+++ src/components/welcome.tsx
@@ -0,0 +1,20 @@
+const Welcome = () => {
+    return (
+        <div className="px-5 py-12">
+            <h1 className="font-bold text-4xl uppercase mb-6">
+                Reabertura <br /> Smart Fit
+            </h1>
+
+            <div className="w-36 h-4 bg-brand-darkGrey mb-8"></div>
+
+            <p>
+                O horário de funcionamento das nossas unidades está seguindo os
+                decretos de cada município. Por isso, confira aqui se a sua
+                unidade está aberta e as medidas de segurança que estamos
+                seguindo.
+            </p>
+        </div>
+    );
+};
+
+export default Welcome;
