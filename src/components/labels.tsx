fbdde7c5cc31a32c25b73b66596d22f72dfbbf2c
Hundlee <mesquitag884@gmail.com>
2024-02-09 15:13:55 -0300

    feat: add Label component

---
+++ src/components/labels.tsx
@@ -0,0 +1,35 @@
+import { LABELS } from "../constants/labels";
+
+const Labels = () => {
+    return (
+        <div className="px-5 mt-5">
+            <div className="px-5 pt-14 py-7 bg-brand-lightGrey bg-opacity-10 flex flex-col gap-12">
+                {LABELS.map((label) => (
+                    <div className="flex flex-col gap-6">
+                        <h3 className="font-semibold text-xl text-center">
+                            {label.description}
+                        </h3>
+
+                        <div className="flex justify-center gap-6">
+                            {label.images.map((images) => (
+                                <div className="flex flex-col items-center">
+                                    <img
+                                        width={75}
+                                        height={75}
+                                        src={images.url}
+                                        alt={images.alt}
+                                    />
+                                    <p className="text-center">
+                                        {images.description}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Labels;
